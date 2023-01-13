32af29ad2cb665421e54e5aa10a4b0ef500a4a30
jacekkawiorski <jkawiorski89@gmail.com>
2023-01-13 21:44:00 +0100

    Add login functionality

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,15 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
 
+  final emailcontroller = TextEditingController();
+  final passwordcontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Jestes niezalogowany'),
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Zaloguj sie do aplikacji"),
+              const SizedBox(
+                height: 50,
+              ),
+              TextField(
+                decoration: const InputDecoration(hintText: 'Email'),
+                controller: emailcontroller,
+              ),
+              TextField(
+                decoration: const InputDecoration(hintText: 'Password'),
+                controller: passwordcontroller,
+                obscureText: true,
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: emailcontroller.text,
+                      password: passwordcontroller.text,
+                    );
+                  } catch (error) {
+                    print(error);
+                  }
+                },
+                child: const Text('Zaloguj sie'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
