1ffa56e058a2051dd49567eb23f574da9ac92919
jacekkawiorski <jkawiorski89@gmail.com>
2023-01-14 18:51:58 +0100

    Implement add restaurants

--- lib/app/home/add_opinon/add_opinion_page_content.dart
+++ lib/app/home/add_opinon/add_opinion_page_content.dart
@@ -1,14 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageCcontent extends StatelessWidget {
+class AddOpinionPageCcontent extends StatefulWidget {
   const AddOpinionPageCcontent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageCcontent> createState() => _AddOpinionPageCcontentState();
+}
+
+class _AddOpinionPageCcontentState extends State<AddOpinionPageCcontent> {
+  var restaurantName = "";
+  var foodName = '';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Ekran z Dodaj'),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Podaj nazwę lokalu',
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  restaurantName = newValue;
+                });
+              },
+            ),
+            const SizedBox(height: 40),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: "Podaj nazwę fast food'a",
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  foodName = newValue;
+                });
+              },
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection('restaurants').add({
+                  'food': foodName,
+                  'name': restaurantName,
+                  'rating': 3.0,
+                });
+              },
+              child: const Text('Dodaj opinie'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
